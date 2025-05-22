a6d9ffcc861b67dad78e734153eede26a1a2a9d3
Be Cappy! <thien.nglee@gmail.com>
2025-05-22 19:33:48 +0700

    Upload

--- src/Components/Pages/ItemDetail/Options.jsx
+++ src/Components/Pages/ItemDetail/Options.jsx
@@ -1,7 +1,29 @@
-function Options() {
+import styles from '../../../Style/Pages/ItemDetail/Options.module.scss';
+
+function Options({ data }) {
     return (
-        <section>
-            <h1>heeh</h1>
+        <section className={styles.options}>
+            <div>
+                {data.storage_options.map((storage, index) => (
+                    <div key={index}>
+                        <strong>{storage[`storage${index + 1}`]}</strong>
+                        <span>{storage[`colors${index + 1}`][0].price1}</span>
+                    </div>
+                ))}
+            </div>
+            <p>Chọn màu để xem giá và chi nhánh có hàng</p>
+            <div>
+                {data.storage_options.map((storage, index) => (
+                    <div key={index}>
+                        <strong>{storage[`storage${index + 1}`]}</strong>
+                        <span>{storage[`colors${index + 1}`][0].price1}</span>
+                    </div>
+                ))}
+            </div>
+            <div>
+                <div></div>
+                <div></div>
+            </div>
         </section>
     );
 }
